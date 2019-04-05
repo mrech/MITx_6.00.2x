9d064648987797ac179c4b904d1c11f098fa7471
mrech <rivato.morena@gmail.com>
2019-04-05 12:30:12 +0200

    Depth_Breadth_First_Search

--- 3.GraphProblems/WeightedGraph.py
+++ 3.GraphProblems/WeightedGraph.py
@@ -1,15 +1,3 @@
-'''
-Consider our representation of permutations of students in 
-a line from Exercise 1. (The teacher only swaps the positions 
-of two students that are next to each other in line.) Let's 
-consider a line of three students, Alice, Bob, and Carol 
-(denoted A, B, and C). Using the Graph class created in the 
-lecture, we can create a graph with the design chosen in 
-Exercise 1: vertices represent permutations of the students 
-in line; edges connect two permutations if one can be made 
-into the other by swapping two adjacent students.
-'''
-
 # Define Class Node (Vertices who represent premutations of students)
 
 
@@ -47,21 +35,23 @@ class Edge(object):
 # weighting edges higher for moves that are harder to make.
 # EG in the student permutations:
 # 1. A large student who is difficult to move around in line
-# 1. weight heavily all edges that involve moving that student
+# 1. weight heavily all edges that involve moving that student (valid for both directions)
 # 2. A sticky spot on the floor which is difficult to move onto and off of
-# 2. weight heavily all edges that involve moving thorugh that point
+# 2. weight heavily all edges that involve moving thorugh that point (valid for both directions)
 
 
 class WeightedEdge(Edge):
     def __init__(self, src, dest, weight):
         self.weight = weight
+        # invoking the __init__ of the parent class
+        Edge.__init__(self, src, dest)
 
     def getWeight(self):
         return self.weight
 
     def __str__(self):
         return self.src.getName() + '->' + self.dest.getName() +\
-            '(' + self.weight + ')'
+            ' (' + str(self.getWeight()) + ')'
 
 # Define Diagraph with an adjacency list implementation.
 # Associate with each node a list of destination nodes
@@ -122,91 +112,17 @@ class Digraph(object):
                     + dest.getName() + '\n'
         return result[:-1]  # omit final newline
 
-# Graph object inherits all attributes and function from Class Digraph
-
-
-class Graph(Digraph):
-    def addEdge(self, edge):
-        Digraph.addEdge(self, edge)
-        # plus it calculates its reverse
-        rev = Edge(edge.getDestination(), edge.getSource())
-        Digraph.addEdge(self, rev)
-# buildPermutationGraph
-
-
-def buildPermutationGraph(graphType):
-    # Instantiate
-    g = graphType()
-    # add nodes
-    nodes = []
-    nodes.append(Node("ABC"))  # nodes[0]
-    nodes.append(Node("ACB"))  # nodes[1]
-    nodes.append(Node("BAC"))  # nodes[2]
-    nodes.append(Node("BCA"))  # nodes[3]
-    nodes.append(Node("CAB"))  # nodes[4]
-    nodes.append(Node("CBA"))  # nodes[5]
-
-    for n in nodes:
-        g.addNode(n)
-
-    # list all possible permutations (undirectional edges)
-    g.addEdge(Edge(g.getNode('ACB'), g.getNode('ABC')))
-    g.addEdge(Edge(g.getNode('BAC'), g.getNode('ABC')))
-    g.addEdge(Edge(g.getNode('BCA'), g.getNode('BAC')))
-    g.addEdge(Edge(g.getNode('CAB'), g.getNode('ACB')))
-    g.addEdge(Edge(g.getNode('CBA'), g.getNode('BCA')))
-    g.addEdge(Edge(g.getNode('CAB'), g.getNode('CBA')))
-
-    return g
-
-
-# Build a Graph
-print(buildPermutationGraph(Graph))
-
-
-g = buildPermutationGraph(Graph)
 
 # Instantiate
-g = Graph()
+g = Digraph()
 # add nodes
 nodes = []
-nodes.append(Node("ABC"))  # nodes[0]
-nodes.append(Node("ACB"))  # nodes[1]
-nodes.append(Node("BAC"))  # nodes[2]
-nodes.append(Node("BCA"))  # nodes[3]
-nodes.append(Node("CAB"))  # nodes[4]
-nodes.append(Node("CBA"))  # nodes[5]
+nodes.append(Node("A"))  # nodes[0]
+nodes.append(Node("B"))  # nodes[1]
 
 for n in nodes:
     g.addNode(n)
 
-# CODE FOR SUBMITION
-# list all possible permutations (bidirectional edges)
-g.addEdge(Edge(g.getNode('ACB'), g.getNode('ABC')))
-g.addEdge(Edge(g.getNode('BAC'), g.getNode('ABC')))
-g.addEdge(Edge(g.getNode('BCA'), g.getNode('BAC')))
-g.addEdge(Edge(g.getNode('CAB'), g.getNode('ACB')))
-g.addEdge(Edge(g.getNode('CBA'), g.getNode('BCA')))
-g.addEdge(Edge(g.getNode('CAB'), g.getNode('CBA')))
-
-
-# Check answers
-edges = g.childrenOf(nodes[0])
-for n in edges:
-    n.getName()
-
-# AUTOMATION
-for i in range(len(nodes)):
-    n = nodes[i].getName()
-    for j in range(i+1, len(nodes)):
-        # if following node names are equal to one of its children
-        if nodes[j].getName() == n[1]+n[0]+n[2] or \
-                nodes[j].getName() == n[0]+n[2]+n[1]:
-            # addEdge
-            g.addEdge(Edge(nodes[i], nodes[j]))
-
-# Check answers
-for z in range(len(nodes)):
-    edges = g.childrenOf(nodes[z])
-    for n in edges:
-        'node[' + str(z) + ']: ' + n.getName()
+# list all possible permutations (unidirectional edges and its weights)
+e = WeightedEdge(g.getNode('A'), g.getNode('B'), 3)
+print(e)
